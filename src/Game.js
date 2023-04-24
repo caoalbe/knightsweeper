be105d5d2d4a36b65765f5fe5f6c0562abcdfec4
Albert Cao <albert.cao.1000@gmail.com>
2023-04-23 23:42:12 -0400

    consisten numeric representations

--- src/Game.js
+++ src/Game.js
@@ -2,13 +2,14 @@ import React, { useState } from "react";
 
 const width = 10;
 const height = 10;
+const tileCount = width * height;
 
 function Game() {
-  // underlying logic
-  const [bombs, setBombs] = useState(new Array(width * height).fill(0)); // 0->8: adjacent. 9: bomb
+  // underlying logic 0->8: adjacent. 9: bomb
+  const [bombs, setBombs] = useState(new Array(tileCount).fill(0));
 
-  // what is shown to user
-  const [visual, setVisual] = useState(new Array(width * height).fill(0)); // 0: blank, 1->8: num, 9: flag, 10: bomb
+  // what is shown to user. 0: blank, 1->8: num, 9: bomb, 10: flag, 11: blank
+  const [visual, setVisual] = useState(new Array(tileCount).fill(11));
 
   // game has started
   const [gameStarted, setGameStarted] = useState(false);
@@ -62,17 +63,34 @@ function generateBombs(index) {
   // naive rng generation
   // place the bombs
   const probability = 0.1;
-  const newBombs = new Array(width * height).fill(0);
-  for (var i = 0; i < width * height; i++) {
+  const newBombs = new Array(tileCount).fill(0);
+  for (var i = 0; i < tileCount; i++) {
     if (i != index && Math.random() < probability) {
       newBombs[i] = 9;
     }
   }
 
   // enumerate the safe tiles
-  var seenTiles = [];
-  for (var i = 0; i < width * height; i++) {
+  var seenTiles;
+  var toCheck;
+  for (var i = 0; i < tileCount; i++) {
     if (newBombs[i] != 9) {
+      seenTiles = [];
+
+      toCheck = [
+        i - 2 * width - 1,
+        i - 2 * width + 1,
+        i - width - 2,
+        i - width + 2,
+        i + 2 * width - 1,
+        i + 2 * width + 1,
+        i + width - 2,
+        i + width + 2,
+      ];
+
+      for (var t = 0; t < toCheck.length; t++) {}
+
+      newBombs[i] = 2;
     }
   }
 
@@ -82,13 +100,11 @@ function generateBombs(index) {
 function rightClick(visual, setVisual, index) {
   const newVisual = visual.map((entry, i) => {
     if (i == index) {
-      if (visual[index] == 0) {
-        return 9; // blank --> flag
-      } else if (visual[index] == 9) {
-        return 0; // flag --> blank
-      } else {
-        return entry;
-      }
+      if (visual[index] == 11) {
+        return 10; // blank --> flag
+      } else if (visual[index] == 10) {
+        return 11; // flag --> blank
+      } else return entry;
     } else return entry;
   });
   setVisual(newVisual);
@@ -103,31 +119,35 @@ function leftClick(
   visual,
   setVisual
 ) {
+  console.log("left start");
+  console.log(index);
   if (!gameStarted) {
     setGameStarted(true);
     const newBombs = generateBombs(index);
     console.log("generations");
     console.log(newBombs);
+    setBombs(newBombs);
   }
-
+  console.log("left middle");
   const newVisual = visual.map((entry, i) => {
     if (i == index) {
-      return 10;
+      return bombs[index];
     } else return entry;
   });
   setVisual(newVisual);
+  console.log("left end");
 }
 
 // 0->8 is revealed
 // 9->11 is unrevealed
 // 0: blank, 1->8: num, 9: flag, 10: bomb
 function numToChar(input) {
-  if (input == 0) {
+  if (input == 11) {
     return "";
   } else if (input == 9) {
-    return "🚩";
-  } else if (input == 10) {
     return "💣";
+  } else if (input == 10) {
+    return "🚩";
   } else {
     return input;
   }
